f0b84c2fffa53d4eab9176df71f1463d66ee6fb1
hector-bueno <hrbueno@gmail.com>
2025-10-05 03:33:26 -0300

    Primeiro Commit Estrutura Básica, Css Básico e JS Toggle Hidden e TrocaArco

---
+++ src/scripts/script.js
@@ -0,0 +1,17 @@
+const buttonToggle = document.getElementById('logo');
+
+function TrocaArco() {
+    const arcoColorido = document.getElementById('arco-externo');
+    const cards = document.querySelectorAll('.cards');
+
+    cards.forEach(card => {
+        // alert(card.classList);
+        card.classList.toggle('hidden');
+        // alert(card.classList);
+    })
+
+    arcoColorido.classList.toggle('active-historia');
+    arcoColorido.classList.toggle('active-respostas');
+
+
+}
